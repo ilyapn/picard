b6e80722ccde67180f289c106041f2295d82ba87
dolga <dolga@DESKTOP-Q6123A7>
2017-03-01 17:01:55 +0300

    Rscript

--- src/main/java/picard/analysis/MeanQualityByCycle.java
+++ src/main/java/picard/analysis/MeanQualityByCycle.java
@@ -203,19 +203,19 @@ public class MeanQualityByCycle extends SinglePassSamProgram {
         if (q.isEmpty() && oq.isEmpty()) {
             log.warn("No valid bases found in input file. No plot will be produced.");
         }
-        else {
-            // Now run R to generate a chart
-            final int rResult = RExecutor.executeFromClasspath(
-                    "picard/analysis/meanQualityByCycle.R",
-                    OUTPUT.getAbsolutePath(),
-                    CHART_OUTPUT.getAbsolutePath(),
-                    INPUT.getName(),
-                    plotSubtitle);
-
-            if (rResult != 0) {
-                throw new PicardException("R script meanQualityByCycle.R failed with return code " + rResult);
-            }
-        }
+//        else {
+//            // Now run R to generate a chart
+//            final int rResult = RExecutor.executeFromClasspath(
+//                    "picard/analysis/meanQualityByCycle.R",
+//                    OUTPUT.getAbsolutePath(),
+//                    CHART_OUTPUT.getAbsolutePath(),
+//                    INPUT.getName(),
+//                    plotSubtitle);
+//
+//            if (rResult != 0) {
+//                throw new PicardException("R script meanQualityByCycle.R failed with return code " + rResult);
+//            }
+//        }
     }
 }
 
